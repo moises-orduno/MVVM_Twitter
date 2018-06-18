88e43bf5f99a73802b8087180af354afe1a84dc7
moisesorduno <morduno@pagatodo.com>
2018-06-18 09:23:58 -0500

    fix size of layout

--- app/src/main/java/com/moisesorduno/twittermvvm/fragment/StatisticsLineFragment.java
+++ app/src/main/java/com/moisesorduno/twittermvvm/fragment/StatisticsLineFragment.java
@@ -2,6 +2,7 @@ package com.moisesorduno.twittermvvm.fragment;
 
 import android.content.Context;
 import android.os.Bundle;
+import android.support.annotation.NonNull;
 import android.support.v4.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
@@ -52,7 +53,7 @@ public class StatisticsLineFragment extends Fragment implements java.util.Observ
     }
 
     @Override
-    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+    public View onCreateView(@NonNull LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         // Inflate the layout for this fragment
 
